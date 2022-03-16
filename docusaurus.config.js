52eb28bc400361d6c6abac126641f26bb778668a
ksokolic <ksenija@atoz.technology>
2022-03-16 14:40:19 +0100

    update config settings with GH data

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,16 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  title: 'Mantil Docs',
+  tagline: 'Mantil is cool!',
+  url: 'https://ksokolic.github.io',
+  baseUrl: '/MantilDocs',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ksokolic', // Usually your GitHub org/user name.
+  projectName: 'MantilDocs', // Usually your repo name.
+  trailingSlash: false,
 
   presets: [
     [
