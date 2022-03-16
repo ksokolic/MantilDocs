80512403d4b42593dce9eabc2d883b8df00b5d4e
ksokolic <ksenija@atoz.technology>
2022-03-16 14:46:18 +0100

    update baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Mantil Docs',
   tagline: 'Mantil is cool!',
   url: 'https://ksokolic.github.io',
-  baseUrl: '/MantilDocs',
+  baseUrl: '/MantilDocs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
